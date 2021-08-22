5e469bf2fc280ff407743ca5f8a8112f8cf088c5
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2021-08-22 07:33:17 +0200

    Changed test class to package private visibility

--- src/test/java/io/github/stevenjdh/simple/ssl/SimpleSSLContextImplTest.java
+++ src/test/java/io/github/stevenjdh/simple/ssl/SimpleSSLContextImplTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 
-public class SimpleSSLContextImplTest extends BaseTestSupport {
+class SimpleSSLContextImplTest extends BaseTestSupport {
     
     private static Method getKeyStoreMethod;
     
