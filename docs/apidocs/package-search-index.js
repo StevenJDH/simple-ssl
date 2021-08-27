8cdab509bd4bd9e7a0e922990fed8ed58ed6afb9
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2021-08-27 09:07:54 +0200

    Added Javadocs to Github Pages

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.stevenjdh.simple.exceptions"},{"l":"io.github.stevenjdh.simple.git"},{"l":"io.github.stevenjdh.simple.security.cert"},{"l":"io.github.stevenjdh.simple.ssl"}];updateSearchResults();
